3aedb60746abf6b3551e379a543b6a638802c6e5
tcmofashi <mofashiforzbx@qq.com>
2025-03-07 00:14:21 +0800

    暂时对抗记忆膨胀

--- src/plugins/chat/emoji_manager.py
+++ src/plugins/chat/emoji_manager.py
@@ -50,8 +50,8 @@ class EmojiManager:
     def __init__(self):
         self.db = Database.get_instance()
         self._scan_task = None
-        self.llm = LLM_request(model=global_config.vlm, temperature=0.3, max_tokens=1000)
-        self.lm = LLM_request(model=global_config.llm_reasoning_minor, max_tokens=1000)
+        self.llm = LLM_request(model=global_config.vlm, temperature=0.3, max_tokens=600)
+        self.lm = LLM_request(model=global_config.llm_reasoning_minor, max_tokens=3000)
         
     def _ensure_emoji_dir(self):
         """确保表情存储目录存在"""
