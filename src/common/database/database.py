0d5e393bcb611a74c8b8941f870f2dabdc6ad54c
墨梓柒 <1787882683@qq.com>
2025-10-31 14:40:27 +0800

    test docker build

--- src/common/database/database.py
+++ src/common/database/database.py
@@ -8,7 +8,7 @@ install(extra_lines=3)
 # 定义数据库文件路径
 ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
 _DB_DIR = os.path.join(ROOT_PATH, "data")
-_DB_FILE = os.path.join(_DB_DIR, "MaiBot.db")
+_DB_FILE = os.path.join(_DB_DIR, "MaiBot.db" )
 
 # 确保数据库目录存在
 os.makedirs(_DB_DIR, exist_ok=True)
